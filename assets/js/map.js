a335d26796110a086ef62eced463e830dc54cc31
RyanSyme <ryu321@hotmail.com>
2020-12-04 09:56:59 +0000

    add hotel names

--- assets/js/map.js
+++ assets/js/map.js
@@ -7,37 +7,43 @@ function initMap(){
 
       let map = new google.maps.Map(document.getElementById('map'), options);
 
-      let hotels = [
+      let markers = [
         {
           coords:{lat: 59.76166893065758, lng: 18.701249380868678},
           iconImage:'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
-          content:'<p>Hotel</p>'
+          content:'<p>Sven Fredriksson Bed & Breakfast</p>'
         },
         {
           coords:{lat: 59.75929151695028, lng: 18.69927527502008},
           iconImage:'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
-          content:'<p>Hotel</p>'
+          content:'<p>Berglings Bed and Breakfast</p>'
         },
         {
           coords:{lat: 59.75847018923395, lng: 18.707257529011045},
           iconImage:'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
-          content:'<p>Hotel</p>'
+          content:'<p>Åtellet Hotell</p>'
         },
         {
           coords:{lat: 59.75601689282998, lng: 18.72249247627313},
           iconImage:'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
-          content:'<p>Hotel</p>'
+          content:'<p>Kärleksudden</p>'
         },
         {
           coords:{lat: 59.755227909002485, lng: 18.69043471395346},
           iconImage:'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
-          content:'<p>Hotel</p>'
+          content:'<p>Norrtälje Vandrarhem Hvilan</p>'
         },
         {
           coords:{lat: 59.75514144388841, lng: 18.71652724343053},
           iconImage:'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
-          content:'<p>Hotel</p>'
+          content:'<p>Pensionat Granparken</p>'
         },
+        {
+          coords:{lat: 59.74709921003163, lng: 18.68682982501255},
+          iconImage:'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
+          content:'<p>Hotell Roslagen</p>'
+        },
+        // Accomodation Markers End //
         {
           coords:{lat: 59.74709921003163, lng: 18.68682982501255},
           iconImage:'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
@@ -46,14 +52,14 @@ function initMap(){
       ];
 
       // Loop through markers
-      for(let i = 0;i < hotels.length;i++){
+      for(let i = 0;i < markers.length;i++){
         // Add marker
-        addMarker(hotels[i]);
+        addMarker(markers[i]);
       }
 
       // Add Marker Function
       function addMarker(props){
-        let hotels = new google.maps.Marker({
+        let markers = new google.maps.Marker({
           position:props.coords,
           map:map,
           //icon:props.iconImage
@@ -62,7 +68,7 @@ function initMap(){
         // Check for customicon
         if(props.iconImage){
           // Set icon image
-          hotels.setIcon(props.iconImage);
+          markers.setIcon(props.iconImage);
         }
 
         // Check content
@@ -71,8 +77,8 @@ function initMap(){
             content:props.content
           });
 
-          hotels.addListener('click', function(){
-            infoWindow.open(map, hotels);
+          markers.addListener('click', function(){
+            infoWindow.open(map, markers);
           });
         }
       }
