390477877682e49c36ba00c7bd1c987bd74abd62
unknown <ryu321@hotmail.com>
2020-12-14 16:09:47 +0100

    fix map function and array

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,39 +1,57 @@
 function initMap() {
-  /* ------------------------------- map options ------------------------------ */
-    let options = {
-      zoom: 14,
-      center: {lat: 59.76234722351998, lng: 18.698235602540187}
-    }
-  /* --------------------------------- new map -------------------------------- */
-    let map = new google.maps.Map(document.getElementById('map'), options);
-  /* ------------------------------- add marker ------------------------------- */
-  // let marker = new google.maps.Marker({
-  //   position: {lat: 59.76234722351998, lng: 18.698235602540187},
-  //   map: map
-  //   // icon: 'assets/images/ban.png'
-  //   });
-  // let infoWindow = new google.maps.InfoWindow({
-  //   content: '<h1>Poop</h1>'
-  //   });
-  
-  // marker.addListener('click', function(){
-  //   infoWindow.open(map, marker);
-  //   });
 
-  /* ------------------------------- add markers ------------------------------ */
-  addMarker({
-    pinpoint:{lat: 59.76234722351998, lng: 18.698235602540187}
-    // iconImage: 'assets/images/ban.png'
-  });
-  addMarker({pinpoint:{lat: 59.76134722351998, lng: 18.698435602540187}});
-  addMarker({pinpoint:{lat: 59.76144772351998, lng: 18.698565602540187}});
+  /* ------------------------------- map options ------------------------------ */
+  let options = {
+      zoom: 14,
+      center: { lat: 59.76234722351998, lng: 18.698235602540187 },
+  };
+
+  /* ------------------------------ generate map ------------------------------ */
+  let map = new google.maps.Map(document.getElementById("map"), options);
+
+  /* ------------------------------ markers array ----------------------------- */
+  let markers = [
+      {
+          coords: { lat: 59.76234722351998, lng: 18.698435602540187 },
+          iconImage: "assets/images/restaurant-pin.png",
+          content: "<h1>Poop</h1>",
+      },
+      {
+          coords: { lat: 59.76734722351998, lng: 18.698235602540187 },
+          content: "<h1>Poop</h1>",
+      },
+      {
+          coords: { lat: 59.76664722351998, lng: 18.698235602540187 },
+      },
+  ];
+
+  /* --------------------------- loop through array --------------------------- */
+  for (let i = 0; i < markers.length; i++) {
+      addMarker(markers[i]);
+  }
+
   /* --------------------------- add marker function -------------------------- */
-  function addMarker(prop){
-    let marker = new google.maps.Marker({
-      position: prop.pinpoint,
-      animation: google.maps.Animation.DROP,
-      map: map,
-      icon: prop.iconImage
-    });
+  function addMarker(props) {
+      let marker = new google.maps.Marker({
+          position: props.coords,
+          map: map,
+      });
+
+      /* ------------------------ check and set custom icon ----------------------- */
+      if (props.iconImage) {
+          marker.setIcon(props.iconImage);
+      }
+
+      /* ------------------------ check for content window ------------------------ */
+      if (props.content) {
+          let infoWindow = new google.maps.InfoWindow({
+              content: props.content,
+          });
+
+          marker.addListener("click", function () {
+              infoWindow.open(map, marker);
+          });
+      }
   }
-  }
+}
+
