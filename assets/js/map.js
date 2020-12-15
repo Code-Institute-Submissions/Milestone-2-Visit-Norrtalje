155578f673b1c896bc32bcf96ff4fb1102590ec6
unknown <ryu321@hotmail.com>
2020-12-15 13:47:17 +0100

    add sport map pins, add extra activities

--- assets/js/map.js
+++ assets/js/map.js
@@ -144,6 +144,12 @@ function initMap() {
         content: "<h1>Sven Fredriksson B&B</h1> <br> <p>Rating:4.4</p>"
       },
       {
+        coords: { lat: 59.76304691004011, lng: 18.6714772427103},
+        type: "Accomodation",
+        iconImage: "assets/images/camping-pin.png",
+        content: "<h1>Norrtälje Camping</h1> <br> <p>Rating:4.4</p>"
+      },
+      {
         coords: { lat: 59.756935432379045, lng: 18.712592123409696},
         type: "Activities",
         iconImage: "assets/images/park-pin.png",
@@ -156,16 +162,46 @@ function initMap() {
         content: "<h1>Kärleksuddens Beach</h1> <br> <p>Rating:3.7</p> <br> <p>Park</p>"
       },
       {
+        coords: { lat: 59.75292287490571, lng: 18.729321035281842},
+        type: "Activities",
+        iconImage: "assets/images/beach-pin.png",
+        content: "<h1>Lommarbadet</h1> <br> <p>Rating:3.9</p> <br> <p>Beach</p>"
+      },
+      {
         coords: { lat: 59.75467589772488, lng: 18.7264464046818},
         type: "Activities",
         iconImage: "assets/images/boat-pin.png",
         content: "<h1>Norrtälje Sailing Club</h1> <br> <p>Rating:4.5</p> <br> <p>Park</p>"
       },
       {
-        coords: { lat: 59.75467589772488, lng: 18.7264464046818},
+        coords: { lat: 59.75711375676702, lng: 18.700849413815916},
         type: "Activities",
         iconImage: "assets/images/boat-pin.png",
         content: "<h1>Norrtälje Town Center</h1> <br> <p>Shopping Area</p>"
+      },
+      {
+        coords: { lat: 59.75467589772488, lng: 18.7264464046818},
+        type: "Activities",
+        iconImage: "assets/images/boat-pin.png",
+        content: "<h1>Norrteljeporten</h1> <br> <p>Shopping Mall</p>"
+      },
+      {
+        coords: { lat: 59.75467589772488, lng: 18.7264464046818},
+        type: "Activities",
+        iconImage: "assets/images/sport-pin.png",
+        content: "<h1>Norrtelje Billiards</h1> <br> <p>Rating:4.7</p> <br> <p>Pool Hall</p>"
+      },
+      {
+        coords: { lat: 59.751897034758116, lng: 18.69764283293879},
+        type: "Activities",
+        iconImage: "assets/images/sport-pin.png",
+        content: "<h1>Norrtälje Sportcentrum</h1> <br> <p>Rating:4.0</p> <br> <p>Sports Complex</p>"
+      },
+      {
+        coords: { lat: 59.7704989090446, lng: 18.69422957505761},
+        type: "Activities",
+        iconImage: "assets/images/sport-pin.png",
+        content: "<h1>Roslagsbowling</h1> <br> <p>Rating:4.4</p> <br> <p>Bowling Alley</p>"
       },
   ];
 
