872d26426381a1cd54f8a286862aeab38219e9f3
RyanSyme <ryu321@hotmail.com>
2021-01-03 15:33:12 +0100

    fix email

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,10 +1,10 @@
 function sendMail(contactForm) {
     let tempParams = {
         from_name: document.getElementById("firstname").value,
-        from_name: document.getElementById("message").value,
+        message: document.getElementById("message").value,
     };
 
-    emailjs.send("outlook","template_vwfbcte", tempParams).then(function(respond){
+    emailjs.send("outlook","template_s7nocct", tempParams).then(function(respond){
         console.log("success",respond.status);
     })
   }
