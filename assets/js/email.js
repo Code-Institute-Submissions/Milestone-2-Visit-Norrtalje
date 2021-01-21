67ea51cbaba3877b5d3c1dc9d849ffbfb8e21ac8
RyanSyme <ryu321@hotmail.com>
2021-01-21 18:58:10 +0000

    add functioning email

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,21 +1,19 @@
 (function() {
-emailjs.init("user_xNMqk1zEghwvFrbJlZbHU");
-})();
+            emailjs.init('user_xNMqk1zEghwvFrbJlZbHU');
+        })();
 
 
-function sendMail(contactForm) {
-    emailjs.send("outlook", "Norrtalje", {
-        "from_name": contactForm.firstname.value,
-        "from_email": contactForm.email.value,
-        "message": contactForm.message.value
-    })
-    .then(
-        function(response) {
-            alert("SUCCESS", response);
-        },
-        function(error) {
-            console.log("FAILED", error);
+ window.onload = function() {
+            document.getElementById('contact-form').addEventListener('submit', function(event) {
+                event.preventDefault();
+                // generate a five digit number for the contact_number variable
+                this.contact_number.value = Math.random() * 100000 | 0;
+                // these IDs from the previous steps
+                emailjs.sendForm('contact_service', 'contact_form', this)
+                    .then(function() {
+                        console.log('SUCCESS!');
+                    }, function(error) {
+                        console.log('FAILED...', error);
+                    });
+            });
         }
-    );
-    return false;  // To block from loading a new page
-}
